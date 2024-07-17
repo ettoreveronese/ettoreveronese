fec57edab2aa420135268c4375dee81a91336732
ettoreveronese <everonese50@gmail.com>
2024-07-17 12:31:30 +0000

    added 3dp link

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 import styles from "./page.module.css";
 import Menu from "./_components/menu/menu.js";
 
-const navbar = [{content: "3dp", link: ""}, {content: "contacts", link: "/ettoreveronese/contacts"}, {content: "about", link: "/ettoreveronese/about"}];
+const navbar = [{content: "3dp", link: "/3dp"}, {content: "contacts", link: "/ettoreveronese/contacts"}, {content: "about", link: "/ettoreveronese/about"}];
 export default function Home() {
   return (
     <main className={styles.main}>
