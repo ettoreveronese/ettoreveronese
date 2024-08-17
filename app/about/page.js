63417f412d66cc760a08a1ff73a529252ad52476
ettoreveronese <everonese50@gmail.com>
2024-08-17 21:04:01 +0000

    new style

--- app/about/page.js
+++ app/about/page.js
@@ -3,7 +3,7 @@ import styles from "./page.module.css";
 export default function About() {
   return (
     <main className={styles.main}>
-      <p className={styles.aboutMe}>Hi, I&apos;m Ettore Veronese, a guy from a small italian town who likes tinkering with technology</p>
+      <p className={styles.aboutMe}>ettore veronese</p>
     </main>
   );
 }
